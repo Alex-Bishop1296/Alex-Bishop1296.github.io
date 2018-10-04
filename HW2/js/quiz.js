9ff4ac571d1f74c0acd0543774a4385e3e184d44
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-03 21:16:46 -0700

    got question adder working

--- HW2/js/quiz.js
+++ HW2/js/quiz.js
@@ -1,10 +1,12 @@
 // A variable to store all quiz images, correct answers, and choices
 var questions = [{
+    prompt: "What bone is this?",
     image: "../img/001.jpg",
     choices: ["Fibula", "Femur", "Tibia", "Tarsus"],
     correctAnswer: 1
 },
 {
+    prompt: "What bone is this?",
     image: "../img/002.jpg",
     choices: ["Vomer", "Ulna", "Humerus", "Femur"],
     correctAnswer: 2
@@ -25,6 +27,7 @@ $(document).ready(function () {
             alert("That name won't do! Fix it ya BONE head!");
         } else {
             startQuiz();
+                nextQuestion();
         }
     });
 
@@ -43,4 +46,21 @@ function startQuiz() {
 
     // Change button prompt
     $("#progression").html("Submit");
+}
+
+// Code for generating next question
+function nextQuestion() {
+    //Assign the questions based on current question
+    var prompt = questions[currentQuestion].prompt;
+    var image = questions[currentQuestion].image;
+    var choice;
+
+    $("#question_prompt").text(prompt);
+    $("#question_image").html('<img src="' + image + '" width="300" height="300">');
+   
+    // For each choice in the current questions choices
+    for (i = 0; i < questions[currentQuestion].choices.length; i++) {
+        choice = questions[currentQuestion].choices[i];
+        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo("#question_choices");
+    }
 }
