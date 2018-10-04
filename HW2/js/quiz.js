6e4370a491dc6f706520607fdd2da349452af937
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-03 18:17:00 -0700

    Implemented document ready foundations

--- HW2/js/quiz.js
+++ HW2/js/quiz.js
@@ -11,7 +11,29 @@ var questions = [{
 }
 ]
 
+//Items we need to keep track of for this game to work
 var playerName = "";
 var currentQuestion = 0;
 var correctQuestions = 0;
 var quizOver = false;
+
+//This will serve as my game controller 
+$(document).ready(function () {
+    $(this).find("#progression").on("click", function () {
+        //Check if the name field entered by the player is valid
+        if (document.getElementById("name").value == "" || document.getElementById("name").value == " " || document.getElementById("name").value == "Enter your name!") {
+            alert("That name won't do! Fix it ya BONE head!");
+        } else {
+            alert("Good, ya did the thing");
+        }
+    });
+});
+
+// This code must be executed each time the start button is pressed
+function startQuiz() {
+    // Log to the console that the quiz is being started
+    console.log("Starting Quiz");
+
+    playerName = document.getElementById("name").value;
+
+}
