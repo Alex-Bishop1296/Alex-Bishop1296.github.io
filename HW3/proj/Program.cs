797582a314fe25f436af7d11d15493cd393b9462
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-10 11:07:27 -0700

    Continued to work on Program.cs, making progress in generateBinaryRepresentation

--- HW3/proj/Program.cs
+++ HW3/proj/Program.cs
@@ -2,11 +2,51 @@
 // Written By: Alex Bishop
 // Last Edit: 10/10/2018
 
+// For linked list generic object
+using System.Collections.Generic;
+// For StringBuilder() and .ToString()
+using System.Text;
+// For Count()
+using System.Linq;
 
 namespace SolHW
 {
+    /// <summary>  
+    ///  Print the binary representation of all numbers from 1 up to n.
+    ///  This is accomplished by using a FIFO queue to perform a level
+    ///  order(i.e.BFS) traversal of a virtual binary tree
+    ///  and then storing each "value" in a list as it is "visited".
+    /// </summary>
     public class Program
     {
+        static LinkedList<string> generateBinaryRepresentationList(int n)
+        {
+            //This starting section of intializing our lists with default constructors
 
+            // Stringbulder is a dynamic object that allows you to expand the
+            // amount of characters in a string
+            // Create an empty queue of strings with which to perfrom the traversal
+            LinkedQueue<StringBuilder> q = new LinkedQueue<StringBuilder>();
+
+            // A list for returning the binary values
+            LinkedList<string> output = new LinkedList<string>();
+
+            // Error checking for bad values
+            if(n < 1)
+            {
+                // binary representation of negative values is not supported
+                // return an empty list
+                return output;
+            }
+
+            // Enqueue the first binary. Use dynamic string to avoid concat
+            q.Push(new StringBuilder("1"));
+
+            // Breadth-first search (BFS)
+            while (n-- > 0)
+            {
+
+            }
+        }
     }
 }
