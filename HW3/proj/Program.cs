aca39f1359392d7e119d32ff4a051ad0749d55f8
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-10 10:49:40 -0700

    Renamed main.cs to Program.cs

---
+++ HW3/proj/Program.cs
@@ -0,0 +1,12 @@
+﻿// Here is my file for Program with the purpose of replicating the code in Main.java but in C#.
+// Written By: Alex Bishop
+// Last Edit: 10/10/2018
+
+
+namespace SolHW
+{
+    public class Program
+    {
+
+    }
+}
