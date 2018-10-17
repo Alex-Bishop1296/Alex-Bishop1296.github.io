fbffa4581f1d8cfd9587e54a55020b34822727f4
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-17 15:49:08 -0700

    calculations and switch statements for converter

--- HW4/proj/Controllers/HomeController.cs
+++ HW4/proj/Controllers/HomeController.cs
@@ -27,9 +27,41 @@ namespace proj.Controllers
             return View();
         }
 
-        // view for my converter page
+
+        [HttpGet]
         public ActionResult Converter()
         {
+            // Create the strings to contain values and notify the debugger that you have done so
+            string usrInput = Request.QueryString["miles"];
+            string radioField = Request.QueryString["units"];
+            System.Diagnostics.Debug.WriteLine("Creating strings fields for input and radios");
+
+            // Convert the user miles to a double
+            double result = Convert.ToDouble(usrInput);
+            System.Diagnostics.Debug.WriteLine("Converting user input to result double");
+
+
+            // Switch statement based on the result of the radio field
+            switch (radioField)
+            {
+                case "millimeters":
+                    result = result * 1609344;
+                    break;
+                case "centimeters":
+                    result = result * 160934.4;
+                    break;
+                case "meters":
+                    result = result * 1609.344;
+                    break;
+                case "kilometers":
+                    result = result * 1.609344;
+                    break;
+                default:
+                    result = -1;
+                    break;
+            }
+            System.Diagnostics.Debug.WriteLine("Converting answer based on radio field");
+
             //Holds title message for the h2 element
             ViewBag.Message = "Convert Miles to Metric";
             return View();
