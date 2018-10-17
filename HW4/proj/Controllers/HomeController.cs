6875cac906396f8ba4d9dedeaf0c03577d0860fe
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-17 16:01:41 -0700

    added results message calculations

--- HW4/proj/Controllers/HomeController.cs
+++ HW4/proj/Controllers/HomeController.cs
@@ -34,33 +34,42 @@ namespace proj.Controllers
             // Create the strings to contain values and notify the debugger that you have done so
             string usrInput = Request.QueryString["miles"];
             string radioField = Request.QueryString["units"];
+            double result = -1;
+            // Debug message
             System.Diagnostics.Debug.WriteLine("Creating strings fields for input and radios");
 
-            // Convert the user miles to a double
-            double result = Convert.ToDouble(usrInput);
-            System.Diagnostics.Debug.WriteLine("Converting user input to result double");
-
-
-            // Switch statement based on the result of the radio field
-            switch (radioField)
+            // Error checking
+            if (usrInput != null)
             {
-                case "millimeters":
-                    result = result * 1609344;
-                    break;
-                case "centimeters":
-                    result = result * 160934.4;
-                    break;
-                case "meters":
-                    result = result * 1609.344;
-                    break;
-                case "kilometers":
-                    result = result * 1.609344;
-                    break;
-                default:
-                    result = -1;
-                    break;
+                // Convert the user miles to a double
+                result = Convert.ToDouble(usrInput);
+                System.Diagnostics.Debug.WriteLine("Converting user input to result double");
+                // Switch statement based on the result of the radio field
+                switch (radioField)
+                {
+                    case "millimeters":
+                        result = result * 1609344;
+                        break;
+                    case "centimeters":
+                        result = result * 160934.4;
+                        break;
+                    case "meters":
+                        result = result * 1609.344;
+                        break;
+                    case "kilometers":
+                        result = result * 1.609344;
+                        break;
+                    default:
+                        result = -1;
+                        break;
+                }
+                // Debug message
+                System.Diagnostics.Debug.WriteLine("Converting answer based on radio field");
             }
-            System.Diagnostics.Debug.WriteLine("Converting answer based on radio field");
+
+            // The output message for the user
+            string statement = usrInput + " is equal to " + Convert.ToString(result) + " " + radioField;
+            ViewBag.statement = statement;
 
             //Holds title message for the h2 element
             ViewBag.Message = "Convert Miles to Metric";
