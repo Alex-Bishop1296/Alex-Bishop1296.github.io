42e6f9f4422f2e90b9c1e7774911970342a48bea
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-18 11:04:32 -0700

    Pattern Checking to textboxes added

--- HW4/proj/Controllers/ColorController.cs
+++ HW4/proj/Controllers/ColorController.cs
@@ -9,7 +9,14 @@ namespace proj.Controllers
     public class ColorController : Controller
     {
         // GET: Color
+        [HttpGet]
         public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string firstColor, string secondColor)
         {
             return View();
         }
