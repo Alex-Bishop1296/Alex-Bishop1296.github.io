a5dfc86e930ecc92650165394a28410747291ba7
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2018-10-27 13:52:07 -0700

    added drop down menu for apartment building names'

--- HW5/ApartmentsWeb/App_Data/up.sql
+++ HW5/ApartmentsWeb/App_Data/up.sql
@@ -17,8 +17,8 @@ GO
 /* Seeds the new table OrderSheets with 5 entries */
 INSERT INTO [dbo].[OrderSheets] (FirstName, LastName, PhoneNumber, ApartmentName, UnitNumber, RequestDetails, Permission, SubmitTime) VALUES
 	 ('James','Sunderland','14996678594','Tranquil Pyramid','43','Lost Key in the Toilet ... again.','1','2018-10-12 09:00:00'),
-	 ('Henry','Townshed','18724789234','Morning Embrace','23','I can not get out of my room.','1','2018-10-12 11:00:00'),
+	 ('Henry','Townshed','18724789234','South Ashfield Heights','302','I can not get out of my room.','1','2018-10-12 11:00:00'),
 	 ('Heather','Mason','13949394943','Wonders Park','89','Faucets are rusted shut, graffiti on bathroom mirrors','1','2018-10-12 19:00:00'),
-	 ('Eileen','Galvin','18724491113','Morning Embrace','22','Please get Henry out of his room.','0','2018-10-12 13:00:00'),
-	 ('Harry','Mason','13949394943','Wonders Park','89','Have you see a missing little girl, about 10 years old, this tall.','0','2018-10-12 21:00:00')
+	 ('Eileen','Galvin','18724491113','South Ashfield Heights','303','Please get Henry out of his room.','0','2018-10-12 13:00:00'),
+	 ('Harry','Mason','13949394943','Wonders Park','89','Daughter keeps hiding in the open ventilation duck, please fix this.','0','2018-10-12 21:00:00')
 GO
